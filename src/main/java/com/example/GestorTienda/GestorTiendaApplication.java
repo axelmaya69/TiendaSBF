a113332f64b25550c20713aaaa9f6f2ce5bd152f
axelmaya69 <axeldbmaya96@gmail.com>
2024-10-24 21:26:43 -0600

    nada

---
+++ src/main/java/com/example/GestorTienda/GestorTiendaApplication.java
@@ -0,0 +1,13 @@
+package com.example.GestorTienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorTiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorTiendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GestorTienda/GestorTiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestorTienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorTiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
