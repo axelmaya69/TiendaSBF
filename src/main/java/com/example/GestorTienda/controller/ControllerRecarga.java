26bb470ba8eed793e9c005980e65e14ae7408ebd
axelmaya69 <axeldbmaya96@gmail.com>
2024-10-25 20:57:34 -0600

    agregando

--- src/main/java/com/example/GestorTienda/controller/ControllerRecarga.java
+++ src/main/java/com/example/GestorTienda/controller/ControllerRecarga.java
@@ -1,4 +1,8 @@
 package com.example.GestorTienda.controller;
 
+import jdk.jfr.Registered;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ControllerRecarga {
 }
