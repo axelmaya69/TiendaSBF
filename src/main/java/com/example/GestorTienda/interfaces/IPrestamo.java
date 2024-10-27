bfd4051f7e0b712b9c45da62c8d1ae1187cc4c93
axelmaya69 <axeldbmaya96@gmail.com>
2024-10-26 21:37:56 -0600

    implementando arquitectura hexagonal

--- src/main/java/com/example/GestorTienda/interfaces/IPrestamo.java
+++
@@ -1,7 +0,0 @@
-package com.example.GestorTienda.interfaces;
-
-import com.example.GestorTienda.domain.models.Prestamo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IPrestamo extends JpaRepository<Prestamo,Integer> {
-}
