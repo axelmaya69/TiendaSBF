62acc6f7175874cf56fcfe78ca3499689e9e0a83
axelmaya69 <axeldbmaya96@gmail.com>
2024-10-26 21:38:21 -0600

    implementando arquitectura hexagonal

--- src/main/java/com/example/GestorTienda/interfaces/IRecarga.java
+++
@@ -1,7 +0,0 @@
-package com.example.GestorTienda.interfaces;
-
-import com.example.GestorTienda.domain.models.Recarga;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IRecarga extends JpaRepository<Recarga,Integer> {
-}
