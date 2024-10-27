9bb45df6fe1fc795cbc86f5f20fa9ee5a3ab6199
axelmaya69 <axeldbmaya96@gmail.com>
2024-10-26 21:38:10 -0600

    implementando arquitectura hexagonal

--- src/main/java/com/example/GestorTienda/interfaces/IProducto.java
+++
@@ -1,7 +0,0 @@
-package com.example.GestorTienda.interfaces;
-
-import com.example.GestorTienda.domain.models.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IProducto extends JpaRepository<Producto,Integer> {
-}
