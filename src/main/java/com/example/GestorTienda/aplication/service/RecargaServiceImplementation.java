6b0c5487b5ea878a0eba500e340ea68b6e0a8dae
axelmaya69 <axeldbmaya96@gmail.com>
2024-10-27 21:30:31 -0600

    implementando api con interfaz e inyeccion de dependencias

--- src/main/java/com/example/GestorTienda/aplication/service/RecargaServiceImplementation.java
+++ src/main/java/com/example/GestorTienda/aplication/service/RecargaServiceImplementation.java
@@ -1,4 +1,52 @@
 package com.example.GestorTienda.aplication.service;
 
-public class RecargaServiceImplementation {
+import com.example.GestorTienda.domain.model.Recarga;
+import com.example.GestorTienda.domain.repository.IRecarga;
+import com.example.GestorTienda.domain.service.IRecargaService;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+public class RecargaServiceImplementation implements IRecargaService {
+
+    private final IRecarga recRep;
+
+    //Inyectando dependencias
+    public RecargaServiceImplementation(IRecarga recRep) {
+        this.recRep = recRep;
+    }
+
+    @Override
+    public Recarga crearRecarga(Recarga recarga) {
+        return recRep.save(recarga);
+
+    }
+
+    @Override
+    public Recarga obtenerRecarga(int id) {
+        return recRep.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Recarga> obtenerRecargas() {
+        return recRep.findAll();
+    }
+
+    @Override
+    public Recarga actualizarRecarga(int id,@RequestBody Recarga recarga) {
+        Recarga updateRecarga = recRep.findById(id).get();
+        if(recRep.existsById(id)){
+            updateRecarga.setFecha(recarga.getFecha());
+            updateRecarga.setMonto(recarga.getMonto());
+            updateRecarga.setPersona(recarga.getPersona());
+            return  recRep.save(updateRecarga);
+        }
+
+        return null;
+    }
+
+    @Override
+    public void eliminarRecarga(int id) {
+    recRep.deleteById(id);
+    }
 }
