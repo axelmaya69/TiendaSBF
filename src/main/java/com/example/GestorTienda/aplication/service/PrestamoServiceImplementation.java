aec90450cf78621540ae1b89753808a645684618
axelmaya69 <axeldbmaya96@gmail.com>
2024-10-27 21:29:28 -0600

    implementando api con interfaz e inyeccion de dependencias

--- src/main/java/com/example/GestorTienda/aplication/service/PrestamoServiceImplementation.java
+++ src/main/java/com/example/GestorTienda/aplication/service/PrestamoServiceImplementation.java
@@ -1,33 +1,50 @@
 package com.example.GestorTienda.aplication.service;
 
 import com.example.GestorTienda.domain.model.Prestamo;
+import com.example.GestorTienda.domain.repository.IPrestamo;
 import com.example.GestorTienda.domain.service.IPrestamoService;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 public class PrestamoServiceImplementation implements IPrestamoService {
+
+    private final IPrestamo presRep;
+
+    public PrestamoServiceImplementation(IPrestamo presRep) {
+        this.presRep = presRep;
+    }
+
+
     @Override
     public Prestamo crearPrestamo(Prestamo prestamo) {
-        return null;
+        return presRep.save(prestamo);
     }
 
     @Override
     public Prestamo obtenerPrestamo(int id) {
-        return null;
+        return presRep.findById(id).orElse(null);
     }
 
     @Override
     public List<Prestamo> obtenerPrestamos() {
-        return List.of();
+        return presRep.findAll();
     }
 
     @Override
-    public Prestamo actualizarProducto(int id, Prestamo prestamo) {
+    public Prestamo actualizarPrestamo(int id,@PathVariable Prestamo prestamo) {
+        Prestamo updatePrestamo = presRep.findById(id).get();
+        if (presRep.existsById(id)){
+            updatePrestamo.setFecha(prestamo.getFecha());
+            updatePrestamo.setDescripcion(prestamo.getDescripcion());
+            updatePrestamo.setPersona(prestamo.getPersona());
+            return presRep.save(updatePrestamo);
+        }
         return null;
     }
 
     @Override
-    public void eliminarPrestamo(int id) {
-
-    }
+        public void eliminarPrestamo(int id) {
+        presRep.deleteById(id);
+        }
 }
