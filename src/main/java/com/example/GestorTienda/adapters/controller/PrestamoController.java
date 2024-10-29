f88193906ec2e02848572a0db8a581ee99358a7d
axelmaya69 <axeldbmaya96@gmail.com>
2024-10-28 21:50:24 -0600

    ultimos cambios

--- src/main/java/com/example/GestorTienda/adapters/controller/PrestamoController.java
+++ src/main/java/com/example/GestorTienda/adapters/controller/PrestamoController.java
@@ -3,7 +3,10 @@ package com.example.GestorTienda.adapters.controller;
 
 import com.example.GestorTienda.domain.model.Prestamo;
 import com.example.GestorTienda.domain.service.IPrestamoService;
-import org.apache.coyote.Response;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,34 +23,67 @@ public class PrestamoController {
     }
 
     @GetMapping("/obtener")
-    public ResponseEntity <List<Prestamo>> obtenerTodos(){
-        List <Prestamo> prestamos = prestamoService.obtenerPrestamos();
-        return ResponseEntity.ok(prestamos);
+    public ResponseEntity <?> obtenerTodos() {
+        try {
+            List<Prestamo> prestamos = prestamoService.obtenerPrestamos();
+            return ResponseEntity.ok(prestamos);
+        } catch (DataAccessException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error al acceder a la base de datos, intente más tarde.");
+        }
+
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error, intentelo más tarde");
+        }
     }
 
     @GetMapping("/obtener/{id}")
-    public ResponseEntity<Prestamo> obtenerPorId(@PathVariable int id){
-    Prestamo obtenerPrestamo = prestamoService.obtenerPrestamo(id);
-    return ResponseEntity.ok(obtenerPrestamo);
+    public ResponseEntity<?> obtenerPorId(@PathVariable int id){
+    try {
+        Prestamo obtenerPrestamo = prestamoService.obtenerPrestamo(id);
+        return ResponseEntity.ok(obtenerPrestamo);
+    }catch (Exception e){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Error, inténtelo más tarde.");
+    }
     }
 
     @PostMapping("/crear")
-    public ResponseEntity<Prestamo> guardarPrestamo(@RequestBody Prestamo prestamo){
-        Prestamo nuevoPrestamo =prestamoService.crearPrestamo(prestamo);
-        return  ResponseEntity.ok(nuevoPrestamo);
+    public ResponseEntity<String> guardarPrestamo(@RequestBody Prestamo prestamo){
+    try {
+        Prestamo nuevoPrestamo = prestamoService.crearPrestamo(prestamo);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Creado con éxito");
+    } catch (DataIntegrityViolationException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("Préstamo ya existente!");
+    }catch (Exception e){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error, " +
+                "intentélo más tarde");
+    }
     }
 
     @PutMapping("/actualizar/{id}")
-    public  ResponseEntity<Prestamo> actualizarPrestamo(@PathVariable int id,
+        public  ResponseEntity<String> actualizarPrestamo(@PathVariable int id,
                                                         @RequestBody Prestamo prestamo){
-        Prestamo actualizarPrestamo = prestamoService.actualizarPrestamo(id,prestamo);
-        return ResponseEntity.ok(actualizarPrestamo);
-    }
+        try {
+            Prestamo actualizarPrestamo = prestamoService.actualizarPrestamo(id, prestamo);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Editado con éxito");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error," +
+                    "inténtelo más tarde.");
+        }
+        }
+
 
     @DeleteMapping("/eliminar/{id}")
-    public ResponseEntity<Void> eliminarPrestamo(@PathVariable int id){
-         prestamoService.eliminarPrestamo(id);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<String> eliminarPrestamo(@PathVariable int id){
+    try {
+        prestamoService.eliminarPrestamo(id);
+        return ResponseEntity.ok("Eliminado exitosamente");
+    } catch (Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error," +
+                "inténtelo más tarde.");
+    }
     }
 
 
