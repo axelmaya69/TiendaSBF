cce4033be880492e34c9279785cb1148d3d6b8f3
axelmaya69 <axeldbmaya96@gmail.com>
2024-10-28 21:49:20 -0600

    ultimos cambios

--- src/main/java/com/example/GestorTienda/domain/repository/IPrestamo.java
+++ src/main/java/com/example/GestorTienda/domain/repository/IPrestamo.java
@@ -4,5 +4,6 @@ package com.example.GestorTienda.domain.repository;
 import com.example.GestorTienda.domain.model.Prestamo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface IPrestamo extends JpaRepository<Prestamo,Integer> {
 }
