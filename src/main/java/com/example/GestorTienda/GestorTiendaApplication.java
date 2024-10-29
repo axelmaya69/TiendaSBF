81826027400cd0d05ecc552e44d8134bc027b083
axelmaya69 <axeldbmaya96@gmail.com>
2024-10-28 21:49:39 -0600

    ultimos cambios

--- src/main/java/com/example/GestorTienda/GestorTiendaApplication.java
+++ src/main/java/com/example/GestorTienda/GestorTiendaApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GestorTiendaApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(GestorTiendaApplication.class, args);
+
+
 	}
 
 }
