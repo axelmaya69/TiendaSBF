fc12a878fb8b5111d40508991e3d1cd9512c18de
axelmaya69 <axeldbmaya96@gmail.com>
2024-10-29 21:37:43 -0600

    agregando cors

--- src/main/java/com/example/GestorTienda/adapters/controller/ProductoController.java
+++ src/main/java/com/example/GestorTienda/adapters/controller/ProductoController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/producto")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ProductoController {
 
     private final IProductoService productoService;
