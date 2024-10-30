b43b97640e732040c36bc074482b8e46f014db75
axelmaya69 <axeldbmaya96@gmail.com>
2024-10-29 21:37:24 -0600

    agregando cors

--- src/main/java/com/example/GestorTienda/adapters/controller/PrestamoController.java
+++ src/main/java/com/example/GestorTienda/adapters/controller/PrestamoController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/fiado")
+@CrossOrigin(origins = "http://localhost:3000")
 public class PrestamoController {
 
     private final IPrestamoService prestamoService;
