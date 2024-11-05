71c3805df2f9d369cdf64bc4bd583b18359639de
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-04 22:01:23 -0600

    agregando buscar por nombre

--- src/main/java/com/example/GestorTienda/adapters/controller/ProductoController.java
+++ src/main/java/com/example/GestorTienda/adapters/controller/ProductoController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/producto")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3001")
 public class ProductoController {
 
     private final IProductoService productoService;
