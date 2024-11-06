181e69407b01a0dc105149d5165fef1f8dd34f29
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-05 22:12:01 -0600

    agregando buscar por nombre

--- src/main/java/com/example/GestorTienda/domain/service/IPrestamoService.java
+++ src/main/java/com/example/GestorTienda/domain/service/IPrestamoService.java
@@ -1,6 +1,7 @@
 package com.example.GestorTienda.domain.service;
 
 import com.example.GestorTienda.domain.model.Prestamo;
+import com.example.GestorTienda.domain.model.Producto;
 
 import java.util.List;
 
@@ -9,6 +10,7 @@ public interface IPrestamoService {
 
     Prestamo crearPrestamo(Prestamo prestamo);
     Prestamo obtenerPrestamo(int id);
+    List<Prestamo> obtenerPorNombre(String nombre);
     List<Prestamo> obtenerPrestamos();
     Prestamo actualizarPrestamo(int id, Prestamo prestamo);
     void eliminarPrestamo(int id);
--- src/main/java/com/example/GestorTienda/domain/service/IRecargaService.java
+++ src/main/java/com/example/GestorTienda/domain/service/IRecargaService.java
@@ -1,5 +1,6 @@
 package com.example.GestorTienda.domain.service;
 
+import com.example.GestorTienda.domain.model.Producto;
 import com.example.GestorTienda.domain.model.Recarga;
 
 import java.util.List;
@@ -9,6 +10,7 @@ public interface IRecargaService {
     Recarga crearRecarga(Recarga recarga);
     Recarga obtenerRecarga(int id);
     List<Recarga> obtenerRecargas();
+    List<Recarga> obtenerPorNombre(String nombre);
     Recarga actualizarRecarga(int id,Recarga recarga);
     void eliminarRecarga(int id);
 
