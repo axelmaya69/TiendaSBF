776db56cf55e9dd91ec63ae5604a1ed2a3279511
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-05 22:10:22 -0600

    agregando buscar por nombre

--- src/main/java/com/example/GestorTienda/domain/repository/IPrestamo.java
+++ src/main/java/com/example/GestorTienda/domain/repository/IPrestamo.java
@@ -2,8 +2,12 @@ package com.example.GestorTienda.domain.repository;
 
 
 import com.example.GestorTienda.domain.model.Prestamo;
+import com.example.GestorTienda.domain.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 
 public interface IPrestamo extends JpaRepository<Prestamo,Integer> {
+    List<Prestamo> findByNombreContaining(String nombre);
 }
