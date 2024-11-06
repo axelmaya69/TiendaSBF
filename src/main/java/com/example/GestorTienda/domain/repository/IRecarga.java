192964d847657f2eaf74130a2e772c17a54fb671
axelmaya69 <axeldbmaya96@gmail.com>
2024-11-05 22:10:43 -0600

    agregando buscar por nombre

--- src/main/java/com/example/GestorTienda/domain/repository/IRecarga.java
+++ src/main/java/com/example/GestorTienda/domain/repository/IRecarga.java
@@ -1,8 +1,12 @@
 package com.example.GestorTienda.domain.repository;
 
 
+import com.example.GestorTienda.domain.model.Producto;
 import com.example.GestorTienda.domain.model.Recarga;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IRecarga extends JpaRepository<Recarga,Integer> {
+    List<Recarga> findByNombreContaining(String nombre);
 }
